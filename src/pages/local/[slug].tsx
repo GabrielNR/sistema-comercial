840c2322db3473a371351f7c848b65cc050d0acc
Gabriel <gabriel@iMac-de-Gabriel.local>
2023-09-06 19:37:18 -0400

    local

---
+++ src/pages/local/[slug].tsx
@@ -0,0 +1,221 @@
+
+import {
+  FaWhatsapp,
+  FaInstagram,
+  FaFacebook,
+  FaPhone
+} from 'react-icons/fa'
+
+import {
+  Container,
+  Header,
+  Content,
+  Service,
+  SocialMedia,
+  Background,
+  ContentMedia,
+  Gallery,
+  Doubts,
+} from '../../styles/local'
+
+
+import { GetStaticPaths, GetStaticProps, NextApiRequest } from 'next';
+import { useRouter } from 'next/router';
+import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client'
+
+export interface LocalProps {
+  local: {
+    slug: string;
+    banner: string;
+    title: string;
+    description_local: string;
+    open: string;
+    exit: string;
+  }
+}
+
+export default function place({ local }: LocalProps){
+  return(
+    <>
+    <Header>
+      <div>
+        <a href="/">
+          <img src="/logo.png" />
+        </a>
+      </div>
+    </Header>
+
+    <Container>
+      <Content>
+        <h1>{local.title}</h1>
+        <p>{local.description_local}</p>
+        <div>
+          <h2>Atendimento:</h2>
+
+          <Service>
+            <div>
+              <span>Domingo</span>
+              <span>Fechado</span>
+            </div>
+
+            <div>
+              <span>Segunda</span>
+              <span>{local.open} - {local.exit}</span>
+            </div>
+
+            <div>
+              <span>Terça</span>
+              <span>{local.open} - {local.exit}</span>
+            </div>
+
+            <div>
+              <span>Quarta</span>
+              <span>{local.open} - {local.exit}</span>
+            </div>
+          </Service>
+
+          <Service>
+            <div>
+              <span>Quinta</span>
+              <span>{local.open} - {local.exit}</span>
+            </div>
+
+            <div>
+              <span>Sexta</span>
+              <span>{local.open} - {local.exit}</span>
+            </div>
+
+            <div>
+              <span>Sábado</span>
+              <span>{local.open} - {local.exit}</span>
+            </div>
+          </Service>
+        </div>
+        <div>
+          <h2>Formas de contato:</h2>
+
+          <SocialMedia>
+            <div style={{
+               background: '#64B161'
+            }}>
+              <FaWhatsapp />
+            </div>
+
+            <div style={{
+              background: '#F9373F'
+            }}>
+              <FaInstagram />
+            </div>
+
+            <div style={{
+              background: '#3B5998'
+            }}>
+              <FaFacebook />
+            </div>
+
+            <div style={{
+              background: '#D54344',
+              width: '166px'
+            }}>
+              <FaPhone />
+              <span>(11) 3598 7815</span>
+            </div>
+          </SocialMedia>
+        </div>
+        <div className='Localizacao'>
+          <h2>Endereço:</h2>
+
+          <p>São Paulo, São Paulo</p>
+          <p>Guilherme Gemalla, Jardim América</p>
+          <p>Nº 260</p>
+        </div>
+
+      </Content>
+      <Background style={{backgroundImage: `url(${local.banner})`}}/>
+    </Container>
+    <ContentMedia>
+      <div>
+      <h1>Youtube</h1>
+      <iframe width="495" height="315"
+        src="https://www.youtube.com/embed/tgbNymZ7vqY">
+        </iframe>
+      </div>
+       
+
+        <Gallery>
+          <h1>Galeria de fotos</h1>
+          <div>
+            <img src='/carregar.png'/>
+            <img src='/carregar.png'/>
+          </div>
+          <div className='gallerySecondary'>
+            <img src='/carregar.png'/>
+            <img src='/carregar.png'/>
+            <img src='/carregar.png'/>
+            <img src='/carregar.png'/>
+          </div>
+        </Gallery>
+      </ContentMedia>
+    
+      <Doubts>
+        {/* <p>Duvidas frequentes</p>
+        <p>Duvidas frequentes</p>
+        <p>Duvidas frequentes</p>
+        <p>Duvidas frequentes</p> */}
+      </Doubts>
+      
+    </>
+  )
+}
+
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient() 
+
+  const local = await prismic.query<any>([
+    Prismic.predicates.at('document.type', 'locale')])
+
+  const paths = local.results.map(local => {
+    return {
+      params: {
+        slug: local.uid
+      }
+    }
+  });
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context: any) => {
+  const prismic = getPrismicClient() 
+
+  const { slug } = context.params
+  console.log(slug)
+  const response = await prismic.getByUID<any>('locale', String(slug), {})
+
+  console.log(JSON.stringify(response, null, 2))
+
+
+
+  const local = {
+    slug: response.uid,
+    banner: response.data.banner_local_vertical.url,
+    title: RichText.asText(response.data.title),
+    description_local: RichText.asText(response.data.description_local),
+    open: response.data.aberto, 
+    exit: response.data.fechado, 
+  }
+
+  console.log(JSON.stringify(local, null, 2))
+
+  return {
+    props: {
+      local
+    }
+  }
+}
---
+++ src/styles/local.ts
@@ -0,0 +1,225 @@
+import styled from "styled-components";
+import { LocalProps } from '../pages/local/[slug]'
+
+export const Header = styled.header`
+  height: 5rem;
+  border-bottom: 1px solid #DCE2E5;
+  background-color: #FFFFFF;
+
+  > div {
+    max-width: 1120px;
+    height: 5rem;
+    margin: 0 auto;
+    padding: 0 2rem;
+
+    display: flex;
+    align-items: center;
+
+    img {
+      height: 42px;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  
+`
+
+export const Container = styled.main`
+  height: 100vh;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+
+`;
+
+export const Content = styled.section`
+  max-width: 560px;
+  margin: 0 auto;
+
+  padding: 0 2rem;
+  h1 {
+    margin-bottom: 16px;
+    margin-top: 16px;
+    font-size: 48px;
+    line-height: 54px;
+    font-weight: 600;
+    font-family: 'montserrat';
+  }
+
+  p{
+    margin-bottom: 12px; 
+    font-size: 20px;
+    line-height: 30px;
+    font-weight: 400;
+    font-family: 'montserrat';
+  }
+
+  h2 {
+    font-size: 24px;
+    line-height: 34px;
+    margin-top: 12px;
+  }
+
+  .Localizacao{
+      height: 200px;
+      /* border: 1px solid #111; */
+      padding: 2px;
+    }
+`;
+
+export const Service = styled.div`
+  display: flex;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 16px;
+
+  div {
+    background: white;
+    margin-top: 16px;
+    border: 1px solid #DCE2E5;
+    border-radius: 8px;
+
+    height: 84px;
+    width: 104px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    /* gap: ; */
+
+    span {
+      font-size: 16px;
+      line-height: 26px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    span > span {
+      font-size: 16px;
+      line-height: 26px;
+      font-weight: bold;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+`
+
+export const SocialMedia = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 16px;
+
+    div {
+      display: flex;
+      height: 47px;
+      width: 47px;
+      justify-content: center;
+      align-items: center;
+      border-radius: 8px;
+      margin-right: 16px;
+      margin-bottom: 24px;
+    }
+
+    svg {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: white;
+      width: 22px;
+      height: 22px;
+    } 
+
+    span {
+      margin-left: 18px;
+      color: white;
+    }
+`;
+
+export const Background = styled.section<LocalProps>`
+  margin-top: -80px;
+  flex: 1;
+  /* background: url('/local/doce_companhiaVerical.png') no-repeat center; */
+  background-size: cover;
+`;
+
+
+export const ContentMedia = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 36px;
+
+  h1 {
+    font-size: 24px;
+    line-height: 34px;
+    margin-bottom: 16px;
+  }
+
+  div {
+    /* max-width: 560px; */
+    margin: 0 auto;
+
+    iframe{
+      margin: 0 auto;
+      width: 495.86px;
+      height: 311px;
+    }
+  }
+
+
+`;
+
+export const Gallery = styled.div`
+  h1 {
+    font-size: 24px;
+    line-height: 34px;
+    margin-bottom: 16px;
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 6px;
+
+    img {
+      width: 287px;
+      margin: 0 auto;
+      border-radius: 4px;
+
+      object-fit: cover;
+    }
+  }
+  
+
+
+  .gallerySecondary {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 6px;
+    margin-top: 6px;
+
+   img {
+    width: 100%;
+    height: 85px;
+    max-width: 560px;
+    margin: 0 auto;
+    border-radius: 4px;
+    object-fit: cover;
+  }
+  }
+`
+
+export const Doubts = styled.div`
+  margin-top: 40px;
+  margin-bottom: 120px;
+  padding: 0 2rem;
+  p{
+    border-bottom: 1px solid #868686;
+    font-size: 16px;
+    line-height: 26px;
+    height: 55px;
+    display: flex;
+    align-items: center;
+  }
+`;
